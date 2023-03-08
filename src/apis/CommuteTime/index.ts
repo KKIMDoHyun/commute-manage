779154dbd64714355d5b1a3dc0c4c91b3056fa1b
KKIMDoHyun <kdh5998@naver.com>
2023-03-08 21:57:07 +0900

    절대경로 재설정

---
+++ src/apis/CommuteTime/index.ts
@@ -0,0 +1,15 @@
+import { supabase } from "@utils/supabase";
+
+export const getArriveTime = async () => {
+    const { data: arriveTime } = await supabase
+        .from("commute_time")
+        .select("arrive_item")
+        .eq(
+            "todayDate",
+            new Date()
+                .toLocaleDateString()
+                .replace(/\./g, "")
+                .replace(/\s/g, "-")
+        );
+    return arriveTime;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@apis", replacement: "/src/apis" },
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@pages", replacement: "/src/pages" },
+            { find: "@routes", replacement: "/src/routes" },
+            { find: "@stores", replacement: "/src/stores" },
+            { find: "@types", replacement: "/src/types" },
+            { find: "@utils", replacement: "/src/utils" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
 });
