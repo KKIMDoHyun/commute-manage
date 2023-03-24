a364ee121bdb0669baccc01e65ed3fc4704fde20
KKIMDoHyun <kdh5998@naver.com>
2023-03-24 17:23:19 +0900

    로그인 페이지 화면 구현(기능X)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 
-import { supabase } from "@/utils/supabase";
-
 export const SignIn = () => {
-    const handleLoginBtn = async () => {
-        try {
-            const { data, error } = await supabase.auth.signInWithOAuth({
-                provider: "github",
-            });
-            console.log("AA", data);
-            if (error) {
-                console.log(error);
-            } else {
-                // navigate("/");
-                console.log(data);
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
     return (
-        <div className="flex flex-col w-full h-full items-center justify-center">
-            <button onClick={handleLoginBtn}>깃헙 로그인</button>
+        <div className="flex flex-1 flex-col w-full h-full items-center justify-center gap-10">
+            <div className="flex">
+                <img src="/public/Tmax.png" />
+            </div>
+            <div className="flex flex-col w-96 h-80 p-4 gap-3">
+                <div className="flex flex-[3] flex-col w-full gap-3">
+                    <input className="flex w-full h-1/4 border-2 border-black rounded-xl" />
+                    <input className="flex w-full h-1/4 border-2 border-black rounded-xl" />
+                    <button className="flex w-full h-1/3 items-center justify-center border-2 bg-[#666666] mt-4">
+                        <span className="font-bold text-white">로그인</span>
+                    </button>
+                </div>
+                <div className="flex flex-[0.5] flex-row w-full gap-3 items-center justify-center">
+                    <input id="saveId" type="checkbox" className="scale-150" />
+                    <label htmlFor="saveId">
+                        <span className="text-lg">ID 저장</span>
+                    </label>
+                    <span className="text-lg">|</span>
+                    <span className="text-lg">회원가입</span>
+                </div>
+            </div>
         </div>
     );
 };
